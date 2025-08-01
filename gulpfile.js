e50f41b6ff44c8a58a2a4fa979bb01d9a04e3c3d
heikodede <heikoler@googlemail.com>
2025-08-01 14:14:02 +0000

    added ai pitchdeck drop in hidden url

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,81 @@
-var gulp = require('gulp');
-var indexInput = require('./indexInput.json');
-var postcss = require('gulp-postcss');
-var handlebars = require('gulp-compile-handlebars');
-var rename = require('gulp-rename');
-var merge = require('merge-stream');
-var fs = require('fs');
-var browserSync = require('browser-sync').create();
-const htmlmin = require('gulp-htmlmin');
+var gulp = require("gulp");
+var indexInput = require("./indexInput.json");
+var postcss = require("gulp-postcss");
+var handlebars = require("gulp-compile-handlebars");
+var rename = require("gulp-rename");
+var merge = require("merge-stream");
+var fs = require("fs");
+var browserSync = require("browser-sync").create();
+const htmlmin = require("gulp-htmlmin");
 
 function build() {
-
     var options = {
-        batch : ['./src/partials'],
-        helpers : {
-            capitals : function(str){
+        batch: ["./src/partials"],
+        helpers: {
+            capitals: function (str) {
                 return str.toUpperCase();
-            }
-        }
+            },
+        },
     };
- 
-    var website_en = gulp.src('./src/index.handlebars')
+
+    var website_en = gulp
+        .src("./src/index.handlebars")
         .pipe(handlebars(indexInput, options))
-        .pipe(rename('index.html'))
+        .pipe(rename("index.html"))
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest("./"));
 
     var legalPayload = {
         general: {
-            showNavItems: false
-        }
+            showNavItems: false,
+        },
     };
-    var legalnotice = gulp.src('./src/legalnotice.handlebars')
+    var legalnotice = gulp
+        .src("./src/legalnotice.handlebars")
         .pipe(handlebars(legalPayload, options))
-        .pipe(rename('legalnotice.html'))
+        .pipe(rename("legalnotice.html"))
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest("./"));
 
-    var dataprivacy = gulp.src('./src/dataprivacy.handlebars')
+    var dataprivacy = gulp
+        .src("./src/dataprivacy.handlebars")
         .pipe(handlebars(legalPayload, options))
-        .pipe(rename('dataprivacy.html'))
+        .pipe(rename("dataprivacy.html"))
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest("./"));
 
-    var droppitchdeck = gulp.src('./src/droppitchdeck.handlebars')
+    var droppitchdeck = gulp
+        .src("./src/droppitchdeck.handlebars")
         .pipe(handlebars(legalPayload, options))
-        .pipe(rename('droppitchdeck.html'))
+        .pipe(rename("droppitchdeck.html"))
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest("./"));
 
-    var portfoliotraining = gulp.src('./src/portfoliotraining.handlebars')
+    var aidroppitchdeck = gulp
+        .src("./src/aidroppitchdeck.handlebars")
         .pipe(handlebars(legalPayload, options))
-        .pipe(rename('portfoliotraining.html'))
+        .pipe(rename("aidroppitchdeck.html"))
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest("./"));
 
-    var postCss = gulp.src('./src/*.css')
-        .pipe(postcss())
-        .pipe(gulp.dest('./'));
+    var portfoliotraining = gulp
+        .src("./src/portfoliotraining.handlebars")
+        .pipe(handlebars(legalPayload, options))
+        .pipe(rename("portfoliotraining.html"))
+        .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+        .pipe(gulp.dest("./"));
 
-    return merge(website_en, legalnotice, dataprivacy, droppitchdeck, portfoliotraining, postCss);
-};
+    var postCss = gulp.src("./src/*.css").pipe(postcss()).pipe(gulp.dest("./"));
+
+    return merge(
+        website_en,
+        legalnotice,
+        dataprivacy,
+        droppitchdeck,
+        portfoliotraining,
+        postCss,
+    );
+}
 
 function reload(done) {
     browserSync.reload();
@@ -69,16 +85,16 @@ function reload(done) {
 function serve(done) {
     browserSync.init({
         server: {
-            baseDir: './'
+            baseDir: "./",
         },
-        open: false
+        open: false,
     });
     done();
 }
 
 const watch = () => {
     gulp.watch(["./src/**", "./assets/**"], gulp.series(build, reload));
-}
+};
 
-gulp.task('default', gulp.series(build, serve, watch));
-gulp.task('build', build);
+gulp.task("default", gulp.series(build, serve, watch));
+gulp.task("build", build);
